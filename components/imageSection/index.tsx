822cd25543c616b4a164bddb0b00ad84d0f5820c
Facundo <facucabrera2004@gmail.com>
2022-04-29 11:09:19 -0300

    Fix of css responsive overflow error

--- components/imageSection/index.tsx
+++ components/imageSection/index.tsx
@@ -5,8 +5,6 @@ export const ImageSection = () => {
     return (
         <section className={styles.section1}>
             <Navbar />
-            <div>
-            </div>
             <div className={styles.divprincipal}>
                 <h2 className={styles.h2}>{'I\'m Facundo Cabrera'}</h2>
                 <h2 className={styles.h2dev}>- Javascript Developer -</h2>
